0ebcc06ec2258926d9997d9d50a3f665c320fd53
bjade <bjade@gracenote.com>
2019-03-20 11:17:38 +0530

    add port

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/get/books', (req, res) => {
   })).pipe(res);
 
 });
-const PORT = 5000;
+const PORT = process.env.PORT || '5000';
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`)
